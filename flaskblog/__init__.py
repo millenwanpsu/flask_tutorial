3691a6f8f3ec2e8439a0cfc9cd1354b61c4037ed
millenwanpsu <mwan@pdx.edu>
2018-11-14 16:02:59 -0800

    Copied Corey Schafer's code from Youtube tutorial up to Part 6

---
+++ flaskblog/__init__.py
@@ -0,0 +1,19 @@
+"""
+Copied from CoreyMSchafer's Flask Tutorial
+https://github.com/CoreyMSchafer/code_snippets/blob/master/Python/Flask_Blog/06-Login-Auth/flaskblog/__init__.py
+"""
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from flaskblog import routes
---
+++ run.py
@@ -0,0 +1,8 @@
+"""
+Copied from CoreyMSchafer's Flask Tutorial
+https://github.com/CoreyMSchafer/code_snippets/blob/master/Python/Flask_Blog/06-Login-Auth/run.py
+"""
+from flaskblog import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
