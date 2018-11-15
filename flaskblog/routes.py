5184f10f1790fd25d0df78a3877b5f95b71c04d4
millenwanpsu <mwan@pdx.edu>
2018-11-14 18:38:15 -0800

    Modified routes to display landpage. Modified landpage to include both login and registration forms.

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -66,11 +66,32 @@ def login():
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
-
 # Millen's Added code for a merged landing page
 @app.route("/landpage", methods=['GET', 'POST'])
 def landpage():
-    return
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form_register = RegistrationForm()
+    form_login = LoginForm()
+    # Registration Form Submitted
+    if form_register.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form_register.password.data).decode('utf-8')
+        user = User(username=form_register.username.data, email=form_registser.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    # Login Form Submitted
+    if form_login.validate_on_submit():
+        user = User.query.filter_by(email=form_login.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form_login.password.data):
+            login_user(user, remember=form_login.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    # Render both
+    return render_template('landpage.html', title='Landing', form_l=form_login, form_r=form_register)
 
 
 @app.route("/logout")
